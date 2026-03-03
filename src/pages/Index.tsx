01a80ad6c50153ab6ae1d1ef54e0394cafbbce50
Lalithaditya <117468638+lalithaditya66@users.noreply.github.com>
2026-03-04 00:11:57 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -94,8 +94,6 @@ const Index = () => {
       <p>
         Threat hunting is a proactive approach for finding sophisticated threats in an organization's environment which evaded automated cyber defense solutions (such as EDR, SIEM, Firewalls, and IDS).
         Hunting not only helps to identify APTs, malware infections, malicious activity etc. within the network, but it also goes much further, uncovering known and unknown security gaps that other teams may have overlooked.
-        Unfortunately, as of today (March 2026), most organizations still take a reactive approach to the extraordinarily fast-paced cyber threat landscape.
-        In this blog, I will be sharing tools, techniques, and concepts that I have learned and implemented during my hunting journey. Stay tuned!
       </p>
 
       <p>
